238d54c913eff9a28e4f1147597a25394a9615ab
benfasoli <benfasoli@gmail.com>
2016-02-25 15:16:00 -0700

    Visualization tools, rstudio addin, documentation

---
+++ R/visualization-tools.R
@@ -0,0 +1,84 @@
+#' Generates interactive ggplot
+#'
+#' \code{iggplot} starts new interactive gadget session to create and view
+#'   the given ggplot object. Returns the zoomed ggplot object when the 
+#'   'Done' button is pressed.
+#'
+#' @param fig ggplot object to be made interactive
+#' @examples
+#' x <- seq(0, 2*pi, length.out=100)
+#' f <- qplot(x, sin(x))
+#' iggplot(f)
+#'                  
+#' @export
+iggplot <- function(fig) {
+  require(ggplot2)
+  require(miniUI)
+  require(shiny)
+  if(!all(class(fig) %in% c('gg','ggplot')))
+    stop('Incorrect fig input. Needs to be a ggplot object.')
+  
+  x.class <- ggplot2::ggplot_build(fig)$panel$x_scales[[1]]$scale_name
+  y.class <- ggplot2::ggplot_build(fig)$panel$y_scales[[1]]$scale_name
+  
+  # Generate Plot UI ----------------------------------------------------------
+  ui <- miniPage(
+    gadgetTitleBar('Drag and double click to zoom', left=NULL),
+    plotOutput('plot', height='100%',
+               dblclick='plot_dblclick',
+               hover=hoverOpts('plot_hover', 
+                               delay=100, delayType='debounce'),
+               brush=brushOpts('plot_brush', resetOnNew=T,
+                               delay=5000, delayType='debounce')),
+    textOutput('hover_vals')
+  )
+  
+  server <- function(input, output) {
+    lim <- reactiveValues(x=NULL, y=NULL)
+    
+    plt <- reactive({
+      newfig <- fig
+      if(!is.null(lim$x))
+        newfig <- newfig + xlim(lim$x)
+      if(!is.null(lim$y))
+        newfig <- newfig + ylim(lim$y)
+      newfig
+    })
+    
+    output$plot <- renderPlot(plt())
+    
+    output$hover_vals <- renderText({
+      if(is.null(input$plot_hover)) return(' ')
+      else {
+        if(x.class=='datetime')
+          x <- as.POSIXct(input$plot_hover$x, origin='1970-01-01')
+        else x <- input$plot_hover$x
+        if(y.class=='datetime')
+          y <- as.POSIXct(input$plot_hover$y, origin='1970-01-01')
+        else y <- input$plot_hover$y
+        
+        return(paste0('x = ', x, '    ', 'y = ', y))}
+    })
+    
+    observeEvent(input$plot_brush, {
+      brush <- input$plot_brush
+      if(!is.null(brush)) {
+        if(x.class=='datetime')
+          lim$x <- as.POSIXct(c(brush$xmin, brush$xmax), origin='1970-01-01')
+        else lim$x <- c(brush$xmin, brush$xmax)
+        lim$y <- c(brush$ymin, brush$ymax)
+      }
+    })
+    
+    observeEvent(input$plot_dblclick, {
+      if(!any(is.null(lim))) {
+        lim$x <- NULL
+        lim$y <- NULL
+      }
+    })
+    
+    observeEvent(input$done, stopApp(plt()))
+  }
+  
+  suppressWarnings(runGadget(ui, server))
+}
--- README.md
+++ README.md
@@ -10,9 +10,10 @@ devtools::install_github('benfasoli/uataq')
 1. `breakstr` splits strings by the given delimiter and returns a data_frame. Lines that do not fit the correct number of columns are omitted.
 2. `calibrate` applies an algorithm for calibrating atmospheric trace gas data by linearly interpolating several reference gas tanks.
 3. `find_neighbor` finds the closest value index in y for each x, using findInterval.
-4. `gps_dir` calculates the direction between GPS coordinates.
-5. `gps_distance` calculates the distances between GPS coordinates.
-6. `gps_speed` calculates the speed in m/s between GPS coordinates.
-7. `na_interp` linearly interpolates NA values found in vector y with respect to dimension x (e.g. timestamp).
-8. `rds2csv` converts .rds files to ASCII tabular representation.
-9. `run_smooth` generates smoothed representation of x.
+4. `iggplot` generates an interactive gadget for a given ggplot object and adds this functionality as a RStudio Addin.
+5. `gps_dir` calculates the direction between GPS coordinates.
+6. `gps_distance` calculates the distances between GPS coordinates.
+7. `gps_speed` calculates the speed in m/s between GPS coordinates.
+8. `na_interp` linearly interpolates NA values found in vector y with respect to dimension x (e.g. timestamp).
+9. `rds2csv` converts .rds files to ASCII tabular representation.
+10. `run_smooth` generates smoothed representation of x.
