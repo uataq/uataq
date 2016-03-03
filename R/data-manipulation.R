e9cdc842f622d4421bb3ce7125237b67b2195477
benfasoli <benfasoli@gmail.com>
2016-03-03 10:51:03 -0700

    Added R-squared statistic in plot annotation.

--- R/data-manipulation.R
+++ R/data-manipulation.R
@@ -152,6 +152,56 @@ calibrate <- function(time, gasm, gask, auto=F, er_tol=0.1, dt_tol=18000)
   return(out)
 }
 
+#' Keeling isotope analysis
+#' 
+#' \code{keeling} performs keeling isotope analysis, generating a linear fit
+#'   and d13C keeling plot.
+#'
+#' @param co2 numeric co2 concentrations, in ppm
+#' @param d13c numeric d13C, in per mille
+#' @param bg_co2 numeric background co2 concentration, in ppm
+#' @param bg_d13c numeric background d13C, in per mille
+#' 
+#' @return list containing (1) \code{$fit} the linear fit object and (2)
+#'   \code{$fig} the ggplot figure.
+#' 
+#' @export
+keeling <- function(co2, d13c, bg_co2=400, bg_d13c=-8)
+{
+  require(dplyr)
+  require(ggplot2)
+  
+  bg_d13c_inv <- 1 / bg_d13c
+  
+  df <- data_frame(co2, 
+                   d13c, 
+                   co2_inv = 1 / co2, 
+                   d13c_inv = 1 / d13c)
+  
+  fit <- with(df, lm(d13c ~ co2_inv))
+  
+  lab <- list(x=min(df$co2_inv) + 0.25 * (max(df$co2_inv) - min(df$co2_inv)),
+              y=max(df$d13c) - 0.2 * (max(df$d13c) - min(df$d13c)),
+              lab=paste(collapse='',
+                        'atop(delta^13 ~ "C = " ~',
+                        signif(coef(fit)[2], 5),
+                        '~ frac(1, CO[2]) ~',
+                        signif(coef(fit)[1], 5),
+                        ', R^2 ~ "=" ~ ', 
+                        signif(summary(fit)$r.squared, 4), ')'))
+  fig <- ggplot(df, aes(x = co2_inv, y = d13c)) + 
+    geom_point() +
+    geom_abline(slope=coef(fit)[2], 
+                intercept=coef(fit)[1], 
+                color='darkorange') +
+    xlab(expression(frac(1, CO[2]) ~ bgroup('[', frac(1, ppm), ']'))) +
+    ylab(expression(delta^13 ~ 'C [\u2030]')) +
+    annotate('text', x=lab$x, y=lab$y, label=lab$lab, parse=T) +
+    theme_classic()
+  print(fig)
+  return(list(fig=fig, fit=fit))
+}
+
 #' Find nearest neighbors
 #' 
 #' \code{find_neighbor} finds the closest value index in y for each x, 
@@ -200,55 +250,4 @@ na_interp <- function(y, x=NULL) {
 #' @export
 run_smooth <- function(x, n=10) {
   return(stats::filter(x, rep(1 / n, n), sides=2))
-}
-
-#' Keeling isotope analysis
-#' 
-#' \code{keeling} performs keeling isotope analysis, generating a linear fit
-#'   and d13C keeling plot.
-#'
-#' @param co2 numeric co2 concentrations, in ppm
-#' @param d13c numeric d13C, in per mille
-#' @param bg_co2 numeric background co2 concentration, in ppm
-#' @param bg_d13c numeric background d13C, in per mille
-#' 
-#' @return list containing (1) \code{$fit} the linear fit object and (2)
-#'   \code{$fig} the ggplot figure.
-#' 
-#'                  
-#' @export
-keeling <- function(co2, d13c, bg_co2=400, bg_d13c=-8)
-{
-  require(dplyr)
-  require(ggplot2)
-  
-  # bg_co2_inv  <- 1 / bg_co2
-  bg_d13c_inv <- 1 / bg_d13c
-  
-  
-  df <- data_frame(co2, 
-                   d13c, 
-                   co2_inv = 1 / co2, 
-                   d13c_inv = 1 / d13c)
-  
-  fit <- with(df, lm(d13c ~ co2_inv))
-  
-  lab <- list(x=min(df$co2_inv) + 0.25 * (max(df$co2_inv) - min(df$co2_inv)),
-              y=max(df$d13c) - 0.2 * (max(df$d13c) - min(df$d13c)),
-              lab=paste(collapse='',
-                        'delta^13 ~ "C = " ~',
-                        signif(coef(fit)[2], 5),
-                        '~ frac(1, CO[2]) ~',
-                        signif(coef(fit)[1], 5)))
-  fig <- ggplot(df, aes(x = co2_inv, y = d13c)) + 
-    geom_point() +
-    geom_abline(slope=coef(fit)[2], 
-                intercept=coef(fit)[1], 
-                color='darkorange') +
-    xlab(expression(frac(1, CO[2]) ~ bgroup('[', frac(1, ppm), ']'))) +
-    ylab(expression(delta^13 ~ 'C [\u2030]')) +
-    annotate('text', x=lab$x, y=lab$y, label=lab$lab, parse=T) +
-    theme_classic()
-  print(fig)
-  return(list(fig=fig, fit=fit))
 }
