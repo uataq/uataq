0f8cb35418ef56eb2e1c73189916fbd966a7fc95
benfasoli <benfasoli@gmail.com>
2017-04-03 11:14:08 -0600

    Replace project name using mustache template and whisker

--- R/stilt_init.r
+++ R/stilt_init.r
@@ -3,22 +3,25 @@
 #'
 #' \code{stilt_init} generates the framework for a new STILT modeling project
 #'
-#' @param name name or location to initialize new project
-#' @param repo 
+#' @param project name or location to initialize new project
+#' @param repo repo to fetch stilt from using \code{git clone repo}
+#' 
+#' @import whisker
 #' @export
 
-stilt_init <- function(name, repo = 'https://github.com/benfasoli/stilt') {
+stilt_init <- function(project, repo = 'https://github.com/benfasoli/stilt') {
   
-  name <- basename(name)
+  project <- basename(project)
   
   system(paste('git clone', repo))
-  system(paste('mv stilt', name))
+  system(paste('mv stilt', project))
   setwd(name)
   system('chmod +x setup')
   system('./setup')
   
   run_stilt <- readLines('r/run_stilt.r')
-  name_idx <- grepl('project <-', run_stilt, fixed = T)
-  run_stilt[name_idx] <- paste0('project <- \'', name, '\'')
+  # name_idx <- grepl('project <-', run_stilt, fixed = T)
+  # run_stilt[name_idx] <- paste0('project <- \'', project, '\'')
+  run_stilt <- whisker::whisker.render(run_stilt, project)
   writeLines(run_stilt, 'r/run_stilt.r')
 }
