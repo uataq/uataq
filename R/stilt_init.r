2c7762e84f4926c6a7f1121a6711e7664c731518
benfasoli <benfasoli@gmail.com>
2017-04-05 17:10:54 -0600

    Modifies working directory as well

--- R/stilt_init.r
+++ R/stilt_init.r
@@ -5,20 +5,22 @@
 #'
 #' @param project name or location to initialize new project
 #' @param repo repo to fetch stilt from using \code{git clone repo}
-#' 
+#'
 #' @import whisker
 #' @export
 
 stilt_init <- function(project, repo = 'https://github.com/benfasoli/stilt') {
-  
+
   project <- basename(project)
-  
+  wd <- dirname(project)
+  if (wd == '.') wd <- getwd()
+
   system(paste('git clone', repo))
   system(paste('mv stilt', project))
   setwd(project)
   system('chmod +x setup')
   system('./setup')
-  
+
   run_stilt <- readLines('r/run_stilt.r')
   # name_idx <- grepl('project <-', run_stilt, fixed = T)
   # run_stilt[name_idx] <- paste0('project <- \'', project, '\'')
