eb175f1be61e76288f0be4fab1a52d81054088d3
benfasoli <benfasoli@gmail.com>
2017-04-13 17:01:39 -0600

    Fixed broken na_interp with respect to index

--- R/na_interp.r
+++ R/na_interp.r
@@ -1,19 +1,24 @@
 #' Linearly interpolate NA values
 #'
 #' \code{na_interp} linearly interpolates NA values found in vector y with
-#'   respect to dimension x (e.g. timestamp).
+#'   respect to index x (e.g. timestamp).
 #'
 #' @param y numeric vector in which to fill bracketed NA values
-#' @param x vector giving dimension of y. NULL if y is equally spaced
+#' @param x vector giving index of y. NULL if y is equally spaced
 #'
 #' @export
-na_interp <- function(y, x=NULL) {
-  if(is.null(x)) x <- 1:length(y)
-  nona  <- which(!is.na(y))
+na_interp <- function (y, x = NULL) {
+  if (is.null(x)) 
+    x <- 1:length(y)
+  nona <- which(!is.na(y))
   start <- head(nona, 1)
-  end   <- tail(nona, 1)
-  if(length(end - start) < 1) return(y)
-  ysub  <- y[start:end]
-  y[start:end] <- approx(x[start:end], ysub, n=length(ysub), method='linear')$y
+  end <- tail(nona, 1)
+  if (length(end - start) < 1) 
+    return(y)
+  xsub <- x[start:end]
+  ysub <- y[start:end]
+  idx <- which(is.na(ysub))
+  ysub[idx] <- approx(xsub, ysub, xout = idx, method = "linear")$y
+  y[start:end] <- ysub
   return(y)
 }
